d7b7593f913ca46a74b9df2f438e6696e2ae966e
DiegoYanez211 <diegoyanezbastidas@gmail.com>
2018-04-23 19:19:15 -0300

    modificado: .angular-cli.json 	modificado: .editorconfig 	modificado: .gitignore 	modificado: README.md 	modificado: e2e/app.e2e-spec.ts 	modificado: e2e/app.po.ts 	modificado: e2e/tsconfig.e2e.json 	modificado: karma.conf.js 	modificado: package-lock.json 	modificado: package.json 	modificado: protractor.conf.js 	modificado: src/app/app.component.css 	modificado: src/app/app.component.html 	modificado: src/app/app.component.spec.ts 	modificado: src/app/app.component.ts 	modificado: src/app/app.module.ts 	modificado: src/assets/.gitkeep 	modificado: src/environments/environment.prod.ts 	modificado: src/environments/environment.ts 	modificado: src/favicon.ico 	modificado: src/index.html 	modificado: src/main.ts 	modificado: src/polyfills.ts 	modificado: src/styles.css 	modificado: src/test.ts 	modificado: src/tsconfig.app.json 	modificado: src/tsconfig.spec.json 	modificado: src/typings.d.ts 	modificado: tsconfig.json 	modificado: tslint.json 	.vscode/ 	src/app/courses.component.ts 	src/app/courses.service.ts 	src/app/favorite/ 	src/app/summary.pipe.ts 	src/assets/fonts/ 	src/assets/glyphicons/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
+
+
+
+import { SummaryPipe } from './summary.pipe';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { CoursesComponent } from './courses.component';
 import { AppComponent } from './app.component';
-
-
+import { CoursesService } from './courses.service';
+import {FormsModule} from '@angular/forms';
+import { FavoriteComponent } from './favorite/favorite.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent,CoursesComponent,SummaryPipe,FavoriteComponent],
+  imports: [BrowserModule,FormsModule],
+  exports: [FavoriteComponent], 
+  bootstrap: [AppComponent],
+  providers: [CoursesService]
 })
 export class AppModule { }
